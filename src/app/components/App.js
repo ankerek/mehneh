3ccfc24ed50c7d95161e00e73465f7b478f0514e
ankerek <ankerek@gmail.com>
2016-10-16 23:54:02 +0200

    add drawer and menu

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter, Match } from 'react-router';
 
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import SettingsIcon from 'material-ui/svg-icons/action/settings';
+import ShoppingBasketIcon from 'material-ui/svg-icons/action/shopping-basket';
+
 import { productsActions } from '../../products';
 
 import Header from './Header';
 import { Products } from '../../products';
+
+const contentStyles = {
+  'padding-left': 250,
+}
 
 @connect(
   state => ({
@@ -22,7 +32,15 @@ export default class App extends Component {
 
     return (
       <div>
-        <Header />
+        <div style={contentStyles}>
+          <Header />
+        </div>
+        <Drawer width={250}>
+          <MenuItem primaryText="Cashdesk" leftIcon={<ShoppingBasketIcon />} />
+          <MenuItem primaryText="Settings" leftIcon={<SettingsIcon />} />
+          <Divider />
+          <MenuItem>Menu Item 3</MenuItem>
+        </Drawer>
         <BrowserRouter>
           <Match pattern="/" component={Products} />
         </BrowserRouter>
