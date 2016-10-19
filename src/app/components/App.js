274f2b68d5c7397ecd8ffd2e8871de384bd74354
Pan Čeperka <johnnychudomsky@gmail.com>
2016-10-19 15:29:20 +0200

    menu item, product compoment

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -8,6 +8,7 @@ import Divider from 'material-ui/Divider';
 import ShoppingBasketIcon from 'material-ui/svg-icons/action/shopping-basket';
 import StorageIcon from 'material-ui/svg-icons/device/storage';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
+import AttachMoneyIcon from 'material-ui/svg-icons/editor/attach-money';
 
 import { productsActions } from '../../products';
 
@@ -32,27 +33,28 @@ export default class App extends Component {
   }
 
   render() {
-
     return (
       <BrowserRouter>
         <div>
           <Drawer width={250}>
             <MenuItem containerElement={<LinkContainer activeOnlyWhenExact to="/" />} primaryText="Cashdesk" leftIcon={<ShoppingBasketIcon />} />
             <MenuItem containerElement={<LinkContainer to="/products" />} primaryText="Products" leftIcon={<StorageIcon />} />
+            <MenuItem primaryText="Orders" leftIcon={<AttachMoneyIcon />} />
             <MenuItem primaryText="Settings" leftIcon={<SettingsIcon />} />
             <Divider />
             <MenuItem><LinkContainer to="/products">Menu Item 3</LinkContainer></MenuItem>
           </Drawer>
-            
+
           <div style={contentStyles}>
             <Header />
-            
+
             <Match exactly pattern="/" component={CashDesk} />
             <Match pattern="/products" component={Products} />
           </div>
         </div>
       </BrowserRouter>
-   );
+    );
+
   }
 }
 
